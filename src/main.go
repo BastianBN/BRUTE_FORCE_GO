17b391222d7ce03ebad7c0d22ab701a807d04c98
Banzai <bastian.norroy@insa-lyon.fr>
2019-10-14 21:00:06 +0200

    Le programme intelligent, qui analyse la taille du mdp à trouver avant de commencer à chercher (j'peux toujours repasser à l'autre version, le texte commenté)

--- src/main.go
+++ src/main.go
@@ -6,62 +6,35 @@ import (
 	"math"
 	"reflect"
 	"time"
+	"unicode/utf8"
 )
 
 var possibilitescarac = "0123456789abcdefghijklmnopqrstuvwxyz"
 
 func main() {
-	//scanner := bufio.NewReader(os.Stdin) //On utilise le paquet bufio pour créer un scanner qui va lire l'entrée dans la console.
-	fmt.Print("Nombre de caractères dans le mdp à cracker : ")
+	/*fmt.Print("Nombre de caractères dans le mdp à cracker : ")
 	var puissance int
 	_, err := fmt.Scanf("%d", &puissance, 64)
-	//input, _ := scanner.ReadString('\n') // \n = utilisateur appuyant sur entrée, on pourrait mettre " " pour espace.
 	if err != nil {
 		print(err)
+	}*/
+	bytesdata, err := ioutil.ReadFile("mdp.txt")
+	fichier := string(bytesdata)
+	if err != nil {
+		fmt.Println(err)
 	}
+	puissance := utf8.RuneCountInString(fichier)
 
 	fmt.Println(puissance)
 	fmt.Println(reflect.TypeOf(puissance))
 
 	possibilites := math.Pow(36, float64(puissance))
 	fmt.Println(possibilites)
-	fmt.Println(possibilitescarac)
-	fmt.Println(reflect.TypeOf(possibilitescarac))
 
-	//fmt.Println(string(caracteres[i]))
-
-	bytesdata, err := ioutil.ReadFile("mdp.txt")
-	fichier := string(bytesdata)
-	if err != nil {
-		fmt.Println(err)
-	}
 	start := time.Now()
 	recherche("", puissance, fichier)
 	tempspasse := time.Since(start)
 	fmt.Println("Temps écoulé : ", tempspasse)
-
-	//fmt.Println(fichier)
-
-	/*var compteur int
-	for i := 0; i < 36; i++ {
-		for j := 0; j < 36; j++{
-			for k := 0; k < 36; k++{
-				for l := 0; l < 36; l++{
-					for m := 0; m < 36; m++ {
-						chaineatester := string(caracteres[i]) + string(caracteres[j]) + string(caracteres[k]) + string(caracteres[l]) + string(caracteres[m])
-						fmt.Println(chaineatester)
-						compteur += 1
-						if chaineatester == fichier {
-							return
-						}
-					}
-				}
-			}
-		}
-	}
-	fmt.Println(compteur)*/
-	//fmt.Scanln()
-
 }
 
 func recherche(chaine string, ncaracatrouver int, contenufichier string) string {
